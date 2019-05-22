443031f91a79652a54e6298901272ad18896c2a5
Anthony Rurup <tcrurup@gmail.com>
2019-05-22 20:13:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     
     current_hour = Time.new.hour
-    if current_hour >= 12
+    if current_hour > 12
       resp.write "Good Afternoon!"
     else 
       resp.write "Good Morning!"
